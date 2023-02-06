f1c3faf45fc18eb6a2cc1b266ee2026117e89787
“602797395@qq.com” <602797395@qq.com>
2023-02-06 10:04:39 +0800

    使用插件之后提交

--- Modbus/Modbus/Form1.Designer.cs
+++ Modbus/Modbus/Form1.Designer.cs
@@ -28,24 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 141);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 297);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(14, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "功能码05-写入线圈";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "功能码01-读取线圈";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(147, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "功能码02-读取离散线圈";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(300, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "功能码03-读取保持寄存器";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(484, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(178, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "功能码04-读取输入寄存器";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(147, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(178, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "功能码06-写入保存寄存器";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,8 +124,13 @@
         }
 
         #endregion
-
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Modbus/Modbus/Form1.cs
+++ Modbus/Modbus/Form1.cs
@@ -1,12 +1,66 @@
-﻿using System.Windows.Forms;
+﻿using NModbus;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace Modbus
 {
     public partial class Form1 : Form
     {
+        private TcpClient client;
+        private IModbusMaster master;
+        private IModbusFactory factory;
+        public CancellationTokenSource tokenSource = new CancellationTokenSource();
         public Form1()
         {
             InitializeComponent();
+            factory = new ModbusFactory();
+            client = new TcpClient();
+            client.Connect("127.0.0.1", 502);
+            master = factory.CreateMaster(client);
+        }
+
+        private void ReadColDatas(bool[] values)
+        {
+            richTextBox1.AppendText(string.Join(",", values) + "\r\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool[] res = master.ReadCoils(1, 1, 5);
+            ReadColDatas(res);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool[] res = master.ReadInputs(1, 0, 6);
+            ReadColDatas(res);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 写线圈
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            master.WriteSingleCoil(1, 1, true);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
